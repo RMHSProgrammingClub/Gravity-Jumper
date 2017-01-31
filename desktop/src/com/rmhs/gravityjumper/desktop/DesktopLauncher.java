f4246b417c212ba2fdb2314c4f098c837010455b
n9Mtq4 <bill.wresnahan@gmail.com>
2017-01-31 14:45:40 -0500

    Initial Commit

---
+++ desktop/src/com/rmhs/gravityjumper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rmhs.gravityjumper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rmhs.gravityjumper.GravityJumper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GravityJumper(), config);
+	}
+}
---
+++ html/src/com/rmhs/gravityjumper/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.rmhs.gravityjumper.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.rmhs.gravityjumper.GravityJumper;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GravityJumper();
+        }
+}
