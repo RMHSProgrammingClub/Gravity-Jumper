4b8a7c10a5582d9a9444766b95db6840767cd4e5
n9Mtq4 <bill.wresnahan@gmail.com>
2017-01-31 14:56:45 -0500

    minor style change

--- desktop/src/com/rmhs/gravityjumper/desktop/DesktopLauncher.java
+++ desktop/src/com/rmhs/gravityjumper/desktop/DesktopLauncher.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.rmhs.gravityjumper.GravityJumper;
 
 public class DesktopLauncher {
+	
 	public static void main (String[] arg) {
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GravityJumper(), config);
+		final LwjglApplication app = new LwjglApplication(new GravityJumper(), config);
+		
 	}
+	
 }
